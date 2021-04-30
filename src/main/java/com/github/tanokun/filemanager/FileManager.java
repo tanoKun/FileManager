4fb80ce2dd33f81c8247d4dd4754372bd195bf73
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2021-04-30 20:58:49 +0900

    first commit

---
+++ src/main/java/com/github/tanokun/filemanager/FileManager.java
@@ -0,0 +1,14 @@
+package com.github.tanokun.filemanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FileManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
